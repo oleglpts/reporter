cab077910012ac538beece65fd9e8e911f9e90c7
Oleg Lupats <oleglupats@gmail.com>
2019-06-25 11:34:38 +0300

    Added callback

--- reporter.py
+++ reporter.py
@@ -8,22 +8,37 @@
 
 import json
 import importlib
+from utils.callback import server
 from utils.helpers import error_handler
+from utils.callback import callback_terminate
 from config import translate as _, logger, cmd_args
+
 
 ########################################################################################################################
 #                                                  Entry point                                                         #
 ########################################################################################################################
 
+
 if __name__ == '__main__':
-    logger.info(_('reporter started'))
+    if cmd_args.callback_url == 'http://localhost:8080' and cmd_args.token:
+        server.start()
+    form = None
+    logger.info('%s, %s: %s' % (_('reporter started'), _('logging level'), cmd_args.log_level))
     logger.info('%s \'%s\'' % (_('report name'), cmd_args.name))
     try:
         with open('%s%s.json' % (cmd_args.reports, cmd_args.name), 'r') as config_file:
             report_config = json.load(config_file)
         form = report_config.get('format', 'csv')
         getattr(importlib.import_module('formats.%s' % form), form)(report_config)
-        logger.info('%s \'%s.%s\'' % (_('output file'), cmd_args.output, form))
-        logger.info(_('reporter ended'))
+        file_path = '%s.%s' % (cmd_args.output, form)
+        logger.info('%s \'%s\'' % (_('output file'), file_path))
+        callback_terminate(0, {'result': file_path, 'message': 'Success'})
+    except FileNotFoundError:
+        logger.error("%s \'%s\' %s" % (_('report'), cmd_args.name, _('does not exists')))
+        callback_terminate(1, {'result': 'empty', 'message': 'Report \'%s\' does not exists' % cmd_args.name})
+    except ModuleNotFoundError as e:
+        logger.error("%s \'%s\' %s" % (_('format'), form, _('not implemented')))
+        callback_terminate(2, {'result': 'empty', 'message': 'Format \'%s\' not implemented' % form})
     except Exception as e:
-        error_handler(logger, e, '', True, True)
+        error_handler(logger, e, '', False, True)
+        callback_terminate(3, {'result': 'empty', 'message': 'Unexpected error: %s' % str(e)})
