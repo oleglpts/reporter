07a4242359c35b326689b5b2a754f498b993c64d
oleg <oleglupats@gmail.com>
2019-07-15 02:10:07 +0300

    release 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class PostInstall(_install):
 
 
 setup(name='db_report',
-      version='0.0.2',
+      version='0.0.3',
       packages=['db_report', 'db_report.config', 'db_report.utils', 'db_report.formats'],
       url='https://github.com/oleglpts/reporter',
       license='MIT',
